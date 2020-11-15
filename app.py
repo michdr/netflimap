9fb343e0aee99ea7491b8e2462fcd208c6f75923
Michael Druk <mich3.dr@gmail.com>
2020-11-15 01:03:44 +0100

    Add datatable + improve misc
    Globals will brake the app, thus caching is necessary - the fastest (but slow) solution was chosen - hidden div.

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import dash
 import dash_core_components as dcc
 import dash_daq as daq
 import dash_html_components as html
+import dash_table
 import pandas as pd
 from dash.dependencies import Input, Output, State
 
@@ -15,7 +16,14 @@ from netflimap.helpers import (SLIDERS, display_by_visibility,
 
 df_netflix = pd.read_csv("data/netflix_dataset.csv")
 df_country_counts_and_titles = get_df_country_counts_and_titles(df_netflix)
-df_filtered_country_counts_and_titles = df_country_counts_and_titles
+NF_DATATABLE_COLUMNS = [
+    "show_id",
+    "type",
+    "title",
+    "release_year",
+    "duration",
+    "country_code",
+]
 
 external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
@@ -26,6 +34,7 @@ app.layout = html.Div(
             "🎥 Netflimap - browse Netflix titles with a map 🎥",
             style={"textAlign": "center"},
         ),
+        html.Div(id="df-filtered", style={"display": "none"}),
         html.Hr(),
         html.Div(
             [
@@ -99,6 +108,48 @@ app.layout = html.Div(
                             ),
                             style={"textAlign": "center"},
                         ),
+                        html.Br(),
+                        html.Div(
+                            dash_table.DataTable(
+                                id="table",
+                                columns=[
+                                    {"name": i, "id": i}
+                                    for i in df_netflix[NF_DATATABLE_COLUMNS].columns
+                                ],
+                                style_data={
+                                    "whiteSpace": "normal",
+                                    "height": "auto",
+                                },
+                                data=df_netflix.to_dict("records"),
+                                tooltip_data=[
+                                    {
+                                        "type": {
+                                            "value": row["listed_in"],
+                                            "type": "markdown",
+                                        },
+                                        "title": {
+                                            "value": f"**Description**: {row['description']}\n\n"
+                                            f"**Cast**: {row['cast']}\n\n"
+                                            f"**Director**: {row['director']}\n\n",
+                                            "type": "markdown",
+                                        },
+                                        "country_code": {
+                                            "value": row["country"]
+                                            if isinstance(row["country"], str)
+                                            else "",
+                                            "type": "markdown",
+                                        },
+                                    }
+                                    for row in df_netflix.to_dict("rows")
+                                ],
+                                row_selectable="single",
+                                sort_action="native",
+                                page_action="native",
+                                filter_action="native",
+                                page_current=0,
+                                page_size=10,
+                            )
+                        ),
                     ],
                     className="six columns",
                 ),
@@ -131,23 +182,31 @@ def show_hide_n_seasons(visibility_state):
         Input("nf-map", "clickData"),
         Input("select-all-countries", "n_clicks"),
         Input("clear-countries", "n_clicks"),
+        Input("df-filtered", "children"),
     ],
     State("countries-dd", "value"),
 )
-def update_selected_countries(click_data, _, __, value):
+def update_selected_countries(click_data, _, __, json_filtered, value):
     changed_id = [p["prop_id"] for p in dash.callback_context.triggered][0]
     if "select-all-countries" in changed_id:
+        df_filtered_netflix = (
+            pd.read_json(json_filtered) if json_filtered else df_netflix
+        )
+        df_filtered_country_counts_and_titles = get_df_country_counts_and_titles(
+            df_filtered_netflix
+        )
         return get_participating_country_codes(df_filtered_country_counts_and_titles)
     elif "clear-countries" in changed_id:
         return []
     elif "nf-map.clickData" in changed_id:
-        participating_country_codes = get_participating_country_codes(
-            df_filtered_country_counts_and_titles
-        )
+        # reading json_filtered is too slow
+        # participating_country_codes = get_participating_country_codes(
+        #     df_filtered_country_counts_and_titles
+        # )
         with suppress(TypeError, IndexError):
             location = click_data["points"][0]["location"]
-            if location not in participating_country_codes:
-                return value
+            # if location not in participating_country_codes:
+            #     return value
             if not value:
                 value = [location]
             elif location in value:
@@ -158,32 +217,37 @@ def update_selected_countries(click_data, _, __, value):
 
 
 @app.callback(
-    Output("nf-map", "figure"),
+    [Output("nf-map", "figure"), Output("df-filtered", "children")],
     [
         Input("apply-filters", "n_clicks"),
-        Input("movie-len-slider", "value"),
-        Input("movies-switch", "on"),
-        Input("n-seasons-slider", "value"),
-        Input("tv-shows-switch", "on"),
-        Input("free-text-search", "value"),
     ],
-    State("nf-map", "figure"),
+    [
+        State("movie-len-slider", "value"),
+        State("movies-switch", "on"),
+        State("n-seasons-slider", "value"),
+        State("tv-shows-switch", "on"),
+        State("free-text-search", "value"),
+        State("nf-map", "figure"),
+    ],
 )
-def update_nf_map(_, movie_len, movies_on, n_seasons, tv_shows_on, free_text, figure):
-    changed_id = [p["prop_id"] for p in dash.callback_context.triggered][0]
-    if "apply-filters" in changed_id:
-        global df_filtered_country_counts_and_titles
-        df_filtered = get_df_nf_filtered(
+def update_nf_map(
+    n_clicks, movie_len, movies_on, n_seasons, tv_shows_on, free_text, figure
+):
+    if n_clicks:
+        df_filtered_netflix = get_df_nf_filtered(
             df_netflix,
             movie_len if movies_on else None,
             n_seasons if tv_shows_on else None,
         )
         df_filtered_country_counts_and_titles = get_df_country_counts_and_titles(
-            df_filtered
+            df_filtered_netflix
         )
-        return get_nf_count_map(df_filtered_country_counts_and_titles)
+        return (
+            get_nf_count_map(df_filtered_country_counts_and_titles),
+            df_filtered_netflix.to_json(),
+        )
     else:
-        return figure
+        return figure, df_netflix.to_json()
 
 
 if __name__ == "__main__":
