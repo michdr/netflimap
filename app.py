fc73789e84280a2bd7997503a0e64fe5375b0b0f
Michael Druk <mich3.dr@gmail.com>
2020-11-15 18:10:36 +0100

    Define server + add title

--- app.py
+++ app.py
@@ -32,6 +32,8 @@ NF_DATATABLE_COLUMNS = [
 
 external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+server = app.server
+app.title = "Netflimap"
 
 
 def get_tooltip_data(df_nf):
